2a6ca7d351f8ee68292e7187b7731dabedd0bdbf
muhammed <muhammed.thasneem@yahoo.com>
2017-02-16 11:05:49 +0530

    Initial commit

---
+++ Driver/app/src/main/java/com/launcher/mummu/driver/Splash.java
@@ -0,0 +1,13 @@
+package com.launcher.mummu.driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
